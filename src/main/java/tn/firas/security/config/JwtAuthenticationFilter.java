12c19f432fcd9f00044c02a7f1b4ed621ace7cd0
hkimifiras <firashkimi709@gmail.com>
2023-11-27 16:34:53 +0100

    create JwtAuthenticationFilter class

---
+++ src/main/java/tn/firas/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package tn.firas.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import org.springframework.lang.NonNull;
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+}
