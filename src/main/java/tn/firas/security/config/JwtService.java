b06f66a8bdecc4ad1c7cc24ab42530407f5360da
hkimifiras <firashkimi709@gmail.com>
2023-11-28 08:13:35 +0100

    extract the the username from the token

--- src/main/java/tn/firas/security/config/JwtService.java
+++ src/main/java/tn/firas/security/config/JwtService.java
@@ -4,10 +4,13 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -15,7 +18,20 @@ public class JwtService {
     private static  final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
     public String extractUsername(String token) {
-        return null;
+
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return buildToken(extraClaims, userDetails, jwtExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
     private Claims extractAllClaims(String token) {
         return Jwts
