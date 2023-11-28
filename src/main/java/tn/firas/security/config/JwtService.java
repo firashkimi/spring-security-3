bb98c734050c748479dcc1602039837875e6f671
hkimifiras <firashkimi709@gmail.com>
2023-11-28 08:46:39 +0100

    Check if the token is Valid

--- src/main/java/tn/firas/security/config/JwtService.java
+++ src/main/java/tn/firas/security/config/JwtService.java
@@ -44,6 +44,19 @@ public class JwtService {
 
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
+
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
